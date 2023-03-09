88e5a10bbb94a7564b102734b23ef37ef34f9933
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-03-09 23:29:22 +0100

    Use Kieker flow record to avoid trace errors

---
+++ start.sh
@@ -0,0 +1,37 @@
+if [ $# -lt 1 ]
+then
+	echo "Please provide IP as parameter!"
+	exit 1
+fi
+
+MY_IP=$1
+MY_FOLDER=$(pwd)/kieker-results/
+
+set -e
+if [ -d $MY_FOLDER ]
+then
+	rm $MY_FOLDER
+fi
+
+mkdir -p $MY_FOLDER
+
+docker run -p 3306:3306 -d teastore-db
+docker run -e "HOST_NAME=$MY_IP" -e "SERVICE_PORT=10000" -p 10000:8080 -d teastore-registry
+docker run -v $MY_FOLDER/teastore-persistence:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$MY_IP" -e "SERVICE_PORT=1111" -e "DB_HOST=$MY_IP" -e "DB_PORT=3306" -p 1111:8080 -d teastore-persistence
+docker run -v $MY_FOLDER/teastore-auth:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$MY_IP" -e "SERVICE_PORT=2222" -p 2222:8080 -d teastore-auth
+docker run --name recommender -v $MY_FOLDER/teastore-recommender:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$MY_IP" -e "SERVICE_PORT=3333" -p 3333:8080 -d teastore-recommender
+docker run -v $MY_FOLDER/teastore-image:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$MY_IP" -e "SERVICE_PORT=4444" -p 4444:8080 -d teastore-image
+docker run -v $MY_FOLDER/teastore-webui:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$MY_IP" -e "SERVICE_PORT=8080" -p 8080:8080 -d teastore-webui
+
+attempt=0
+while [ $attempt -le 59 ]; do
+    attempt=$(( $attempt + 1 ))
+    echo "Waiting for Tomcat to be up (attempt: $attempt)..."
+    result=$(docker logs recommender 2>&1)
+    if grep -q 'org.apache.catalina.startup.Catalina.start Server startup in ' <<< $result ; then
+      echo "Tomcat is up!"
+      break
+    fi
+    sleep 2
+done
+
---
+++ startJmeter.sh
@@ -0,0 +1,16 @@
+if [ "$JMETER_HOME" == "" ] || [ ! -d $JMETER_HOME ] 
+then
+	echo "\$JMETER_HOME needs to be a directory!"
+	exit 1
+fi
+
+java -jar $JMETER_HOME/bin/ApacheJMeter.jar -t examples/jmeter/teastore_browse_nogui.jmx -Jhostname localhost -Jport 8080 -JnumUser 10 -n 
+
+docker ps | awk '{print $1}' | xargs docker stop $1
+docker ps -a | awk '{print $1}' | xargs docker rm -f $1
+
+sudo chown reichelt:reichelt kieker-results -R 
+echo "Parameters: "
+cd kieker-results && for file in $(echo */*); do echo -n "$(pwd)/$file "; done; echo
+
+
