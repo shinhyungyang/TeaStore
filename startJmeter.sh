a85f0736bc0dde3e068eb8fbdee7c28dd89d1f6a
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-08-06 16:09:50 +0200

    Only stop jmeter images

--- startJmeter.sh
+++ startJmeter.sh
@@ -8,8 +8,8 @@ fi
 
 java -jar $JMETER_HOME/bin/ApacheJMeter.jar -t examples/jmeter/teastore_browse_nogui.jmx -Jhostname localhost -Jport 8080 -n 
 
-docker ps | awk '{print $1}' | xargs docker stop $1
-docker ps -a | awk '{print $1}' | xargs docker rm -f $1
+docker ps | grep teastore | awk '{print $1}' | xargs docker stop $1
+docker ps -a | grep teastore | awk '{print $1}' | xargs docker rm -f $1
 
 USER=$(whoami)
 sudo chown $USER:$USER kieker-results -R 
