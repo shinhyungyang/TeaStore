f498a149a685beecfa7bf203006f138fc34098c6
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-08-08 17:47:54 +0200

    Add script for remote experiment execution

---
+++ startRemoteExperiments.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -lt 1 ]
+then
+	echo "Please provide IP as parameter!"
+	exit 1
+fi
+
+if [ "$JMETER_HOME" == "" ] || [ ! -d $JMETER_HOME ] 
+then
+	echo "\$JMETER_HOME needs to be a directory!"
+	exit 1
+fi
+
+TEASTORE_RUNNER_IP=$1
+if [ $# -gt 1 ]
+then
+	HOST_SELF_IP=$2
+else
+	HOST_SELF_IP=$TEASTORE_RUNNER_IP
+fi
+
+# Just test connection
+ssh -q $1 "exit"
+
+ssh $TEASTORE_RUNNER_IP "if [ ! -d TeaStore ]; then git clone https://github.com/DaGeRe/TeaStore.git; fi"
+ssh $TEASTORE_RUNNER_IP "cd TeaStore; git checkout kieker-debug; git pull"
+
+ssh $TEASTORE_RUNNER_IP "cd TeaStore; ls; ./start.sh $HOST_SELF_IP"
+
+echo
+echo
+echo "Building is finished; Starting load test"
+
+java -jar $JMETER_HOME/bin/ApacheJMeter.jar -t examples/jmeter/teastore_browse_nogui.jmx -Jhostname $TEASTORE_RUNNER_IP -Jport 8080 -n 
+
+echo
+echo
+echo "Load test is finished; Removing containers"
+
+ssh $TEASTORE_RUNNER_IP 'docker ps -a | grep \"teastore\|recommender\" | awk '{print $1}' | xargs docker rm -f $1'
