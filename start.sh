82654ac09cda6d9673e45677b46fe40c1223bf59
DaGeRe <davidgeorg_reichelt@dagere.de>
2024-08-27 15:49:36 +0200

    Correctly write pid

--- start.sh
+++ start.sh
@@ -98,7 +98,7 @@ then
 	java -jar utilities/receiver.jar 10001 > "kieker-receiver.log" &
 	RECEIVER_PID=$!
 	disown
-	echo "$RECEIVER_PID > utilities/receiver.pid"
+	echo "$RECEIVER_PID" > utilities/receiver.pid
 	
 	sed -i "s/kieker.monitoring.writer=kieker.monitoring.writer.filesystem.FileWriter/#kieker.monitoring.writer=kieker.monitoring.writer.filesystem.FileWriter/g" utilities/tools.descartes.teastore.dockerbase/kieker.monitoring.properties
 	sed -i "s/#kieker.monitoring.writer=kieker.monitoring.writer.tcp.SingleSocketTcpWriter/kieker.monitoring.writer=kieker.monitoring.writer.tcp.SingleSocketTcpWriter/g" utilities/tools.descartes.teastore.dockerbase/kieker.monitoring.properties
