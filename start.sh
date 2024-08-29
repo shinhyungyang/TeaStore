d0e009dd0ab10609351e4d313982185ba85207f2
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-08-29 14:34:28 +0200

    Prepare for missing configurations

--- start.sh
+++ start.sh
@@ -114,7 +114,24 @@ then
 	sed -i "s/kieker.monitoring.writer=kieker.monitoring.writer.filesystem.FileWriter/#kieker.monitoring.writer=kieker.monitoring.writer.filesystem.FileWriter/g" utilities/tools.descartes.teastore.dockerbase/kieker.monitoring.properties
 	sed -i "s/#kieker.monitoring.writer=kieker.monitoring.writer.tcp.SingleSocketTcpWriter/kieker.monitoring.writer=kieker.monitoring.writer.tcp.SingleSocketTcpWriter/g" utilities/tools.descartes.teastore.dockerbase/kieker.monitoring.properties
 	sed -i "s/kieker.monitoring.writer.tcp.SingleSocketTcpWriter.hostname=localhost/kieker.monitoring.writer.tcp.SingleSocketTcpWriter.hostname=$1/g" utilities/tools.descartes.teastore.dockerbase/kieker.monitoring.properties
-	
+fi
+
+if [[ "$2" == "BYTEBUDDY" ]]
+then
+	echo "Not implemented yet."
+	exit 1
+fi
+
+if [[ "$2" == "OPENTELEMETRY_DEACTIVATED" ]]
+then
+	echo "Not implemented yet."
+	exit 1
+fi
+
+if [[ "$2" == "OPENTELEMETRY_SPANS" ]]
+then
+	echo "Not implemented yet."
+	exit 1
 fi
 
 
