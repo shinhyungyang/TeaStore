4d0a4aef8558fb565c8c1072efdc918b107b907a
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-08-29 16:57:46 +0200

    Really add Otel

--- functions.sh
+++ functions.sh
@@ -58,3 +58,35 @@ function removeAllInstrumentation {
        rm utilities/tools.descartes.teastore.dockerbase/kieker-*
        cp no-instrumentation-sources/* utilities/tools.descartes.teastore.registryclient/src/main/java/tools/descartes/teastore/registryclient/rest/
 }
+
+function instrumentForOpenTelemetry {
+	MY_IP=$1
+	DEACTIVATED=$2
+
+	if [ ! -f utilities/tools.descartes.teastore.dockerbase/opentelemetry-javaagent.jar ]
+	then
+		curl -L -O https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
+		mv opentelemetry-javaagent.jar utilities/tools.descartes.teastore.dockerbase/opentelemetry-javaagent.jar
+	fi
+	
+	git checkout -- utilities/tools.descartes.teastore.dockerbase/Dockerfile
+	sed -i '/^COPY kieker-2.0.0-SNAPSHOT-aspectj\.jar/d' utilities/tools.descartes.teastore.dockerbase/Dockerfile
+	sed -i '/^COPY kieker\.monitoring\.properties/d' utilities/tools.descartes.teastore.dockerbase/Dockerfile
+	sed -i '/^COPY aop\.xml/d' utilities/tools.descartes.teastore.dockerbase/Dockerfile
+	
+	sed -i '/^EXPOSE 8080/i COPY opentelemetry-javaagent.jar \/opentelemetry\/agent\/agent.jar' utilities/tools.descartes.teastore.dockerbase/Dockerfile
+	git checkout -- utilities/tools.descartes.teastore.dockerbase/start.sh
+	
+	if [[ "$2" != "DEACTIVATED" ]]
+	then
+		sed -i 's|-javaagent:/kieker/agent/agent.jar|-javaagent:/opentelemetry/agent/agent.jar -Dotel.metrics.exporter=none -Dotel.exporter.otlp.endpoint=http://'$MY_IP':4318|g' utilities/tools.descartes.teastore.dockerbase/start.sh
+	
+		docker run -d \
+			--name teastore-otel-collector \
+			-p 4318:4318 \
+			otel/opentelemetry-collector-contrib:0.108.0
+	else
+		sed -i 's|-javaagent:/kieker/agent/agent.jar|-javaagent:/opentelemetry/agent/agent.jar -Dotel.metrics.exporter=none -Dotel.metrics.exporter=none|g' utilities/tools.descartes.teastore.dockerbase/start.sh
+	fi
+		
+}
