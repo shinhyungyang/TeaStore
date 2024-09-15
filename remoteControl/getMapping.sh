5ca2745ccd06fab86e70ed787a4b447e92205a73
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-09-15 21:31:13 +0200

    Add getMapping script

---
+++ remoteControl/getMapping.sh
@@ -0,0 +1,7 @@
+methods=$(cat kieker-results/teastore-*/kieker*/*dat | grep -v "RegistryClient\$1.<init>" | grep -v "2.0.0-SNAPSHOT" | awk -F';' '{print $3}' | awk '{print $NF}' | sort | uniq)
+
+for method in $methods
+do
+	echo -n "$method "
+	cat kieker-results/teastore-*/kieker*/*dat | grep -v "RegistryClient\$1.<init>" | awk -F';' '{print $3}' | grep $method | wc -l
+done
