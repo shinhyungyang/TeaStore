2e60f8ede32c37768778ef2c60cbe7d091cdd836
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-09-16 10:34:58 +0200

    Make startCallAnalysis runnable without sudo

--- remoteControl/startCallAnalysis.sh
+++ remoteControl/startCallAnalysis.sh
@@ -3,13 +3,7 @@
 source 'functions.sh'
 
 function getMapping {
-	methods=$(cat ../kieker-results/teastore-*/kieker*/*dat | grep -v "RegistryClient\$1.<init>" | grep -v "2.0.0-SNAPSHOT" | awk -F';' '{print $3}' | awk '{print $NF}' | sort | uniq)
-
-	for method in $methods
-	do
-		echo -n "$method "
-		cat ../kieker-results/teastore-*/kieker*/*dat | grep -v "RegistryClient\$1.<init>" | awk -F';' '{print $3}' | grep $method | wc -l
-	done
+	docker run --rm -v $(pwd)/../kieker-results:/kieker-results -v $(pwd):/remoteControl fedora:latest bash -c "cd remoteControl && ./getMapping.sh"
 }
 
 function stopTeaStore {
