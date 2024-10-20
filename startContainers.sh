8eb79225e214b879f307d8257ed4f6b9532e3de3
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-10-20 21:38:07 +0200

    Pass correct hostname for distributed persistence service

--- startContainers.sh
+++ startContainers.sh
@@ -138,15 +138,17 @@ docker run --hostname=teastore-webui-$container_id \
 else
 	AGENT_IP=$3
 	docker run --hostname=teastore-persistence-$container_id \
-		-v $MY_FOLDER/teastore-persistence:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$MY_IP" -e "SERVICE_PORT=1111" -e "DB_HOST=$MY_IP" -e "DB_PORT=3306" -p 1111:8080 -d teastore-persistence
+		-v $MY_FOLDER/teastore-persistence:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$agent_ip" -e "SERVICE_PORT=1111" -e "DB_HOST=$MY_IP" -e "DB_PORT=3306" -p 1111:8080 -d teastore-persistence
 	docker run --hostname=teastore-auth-$container_id \
 		-v $MY_FOLDER/teastore-auth:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$agent_ip" -e "SERVICE_PORT=2222" -p 2222:8080 -d teastore-auth
 	docker run --hostname=teastore-recommender-$container_id \
 		-v $MY_FOLDER/teastore-recommender:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$agent_ip" -e "SERVICE_PORT=3333" -p 3333:8080 -d teastore-recommender
 	docker run --hostname=teastore-image-$container_id \
 		-v $MY_FOLDER/teastore-image:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$agent_ip" -e "SERVICE_PORT=4444" -p 4444:8080 -d teastore-image
-	docker run --hostname=teastore-webui-$container_id \
-		-v $MY_FOLDER/teastore-webui:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$agent_ip" -e "SERVICE_PORT=8080" -p 8080:8080 -d teastore-webui
+	
+	# WebUI seems to be current not distributed
+	#docker run --hostname=teastore-webui-$container_id \
+	#	-v $MY_FOLDER/teastore-webui:/kieker/logs/ -e "LOG_TO_FILE=true" -e "REGISTRY_HOST=$MY_IP" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$agent_ip" -e "SERVICE_PORT=8080" -p 8080:8080 -d teastore-webui
 fi
 
 recommender_id=$(docker ps | grep "recommender" | awk '{print $1}')
