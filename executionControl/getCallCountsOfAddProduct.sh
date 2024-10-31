4bc800392298a393c0ad12b32cfca273c9e72922
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-10-31 22:06:47 +0100

    Implement getting external call count

--- executionControl/getCallCountsOfAddProduct.sh
+++ executionControl/getCallCountsOfAddProduct.sh
@@ -1,7 +1,27 @@
-traceIds=$(cat $1/teastore-*/*/*.dat | grep -v "RegistryClient\$1.<init>" | grep CartActionServlet -A 1 | awk -F';' '{print $5}')
+function analyzeCallCounts {
+	FOLDER=$1
+	SERVLET=$2
+	# for call counts, we need grep $SERVLET -A 1
+	traceIds=$(cat $FOLDER/teastore-*/*/*.dat | grep -v "RegistryClient\$1.<init>" | grep $SERVLET | awk -F';' '{print $5}')
 
-for traceId in $traceIds
+	echo -n "#Service Call Counts: "
+	echo $traceIds | awk '{print NF}'
+
+	#for traceId in $traceIds
+	#do
+	#	echo -n "$traceId "
+	#	cat $1/teastore-*/*/*.dat | grep $traceId | wc -l
+	#done | awk '{if (NR % 2 == 0) {print prev + $2} prev=$2;}'
+
+	for traceId in $traceIds
+	do
+		echo -n "$traceId "
+		grep $traceId $1/teastore-*/*/*.dat | awk -F';' '{print $8}' | uniq | wc -l
+	done
+}
+
+for servlet in IndexServlet LoginActionServlet CartServlet CartActionServlet CategoryServlet ProductServlet
 do
-	echo -n "$traceId "
-	cat $1/teastore-*/*/*.dat | grep $traceId | wc -l
-done | awk '{if (NR % 2 == 0) {print prev + $2} prev=$2;}'
+	echo "Analyzing: $servlet"
+	analyzeCallCounts $1 $servlet &> $servlet.txt
+done
