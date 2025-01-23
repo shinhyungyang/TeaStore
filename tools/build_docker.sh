388a8ff8bd7795c5d96f51775212dd325f962d49
Shinhyung Yang <shinhyung.yang@email.uni-kiel.de>
2025-01-23 07:24:42 +0100

    Modified TeaStore Dockerfile scripts
    Modified TeaStore Dockerfile scripts for ICPE25 tool artifact demo

--- tools/build_docker.sh
+++ tools/build_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-push_flag='false'
-registry=''     # e.g. 'descartesresearch/'
+push_flag='true'
+registry='kieker/'     # e.g. 'descartesresearch/'
+tag=':kieker-2.0.2'
 
 print_usage() {
   printf "Usage: docker_build.sh [-p] [-r REGISTRY_NAME]\n"
@@ -15,26 +16,26 @@ while getopts 'pr:' flag; do
   esac
 done
 
-docker build -t "${registry}teastore-db" ../utilities/tools.descartes.teastore.database/
-docker build -t "${registry}teastore-kieker-rabbitmq" ../utilities/tools.descartes.teastore.kieker.rabbitmq/
-docker build -t "${registry}teastore-base" ../utilities/tools.descartes.teastore.dockerbase/
-perl -i -pe's|.*FROM descartesresearch/|FROM '"${registry}"'|g' ../services/tools.descartes.teastore.*/Dockerfile
-docker build -t "${registry}teastore-registry" ../services/tools.descartes.teastore.registry/
-docker build -t "${registry}teastore-persistence" ../services/tools.descartes.teastore.persistence/
-docker build -t "${registry}teastore-image" ../services/tools.descartes.teastore.image/
-docker build -t "${registry}teastore-webui" ../services/tools.descartes.teastore.webui/
-docker build -t "${registry}teastore-auth" ../services/tools.descartes.teastore.auth/
-docker build -t "${registry}teastore-recommender" ../services/tools.descartes.teastore.recommender/
-perl -i -pe's|.*FROM '"${registry}"'|FROM descartesresearch/|g' ../services/tools.descartes.teastore.*/Dockerfile
+docker build -t "${registry}teastore-db${tag}" ../utilities/tools.descartes.teastore.database/
+docker build -t "${registry}teastore-kieker-rabbitmq${tag}" ../utilities/tools.descartes.teastore.kieker.rabbitmq/
+docker build -t "${registry}teastore-base${tag}" ../utilities/tools.descartes.teastore.dockerbase/
+#perl -i -pe's|.*FROM descartesresearch/|FROM '"${registry}"'|g' ../services/tools.descartes.teastore.*/Dockerfile
+docker build -t "${registry}teastore-registry${tag}" ../services/tools.descartes.teastore.registry/
+docker build -t "${registry}teastore-persistence${tag}" ../services/tools.descartes.teastore.persistence/
+docker build -t "${registry}teastore-image${tag}" ../services/tools.descartes.teastore.image/
+docker build -t "${registry}teastore-webui${tag}" ../services/tools.descartes.teastore.webui/
+docker build -t "${registry}teastore-auth${tag}" ../services/tools.descartes.teastore.auth/
+docker build -t "${registry}teastore-recommender${tag}" ../services/tools.descartes.teastore.recommender/
+#perl -i -pe's|.*FROM '"${registry}"'|FROM descartesresearch/|g' ../services/tools.descartes.teastore.*/Dockerfile
 
 if [ "$push_flag" = 'true' ]; then
-  docker push "${registry}teastore-db"
-  docker push "${registry}teastore-kieker-rabbitmq"
-  docker push "${registry}teastore-base"
-  docker push "${registry}teastore-registry"
-  docker push "${registry}teastore-persistence"
-  docker push "${registry}teastore-image"
-  docker push "${registry}teastore-webui"
-  docker push "${registry}teastore-auth"
-  docker push "${registry}teastore-recommender"
+  docker push "${registry}teastore-db${tag}"
+  docker push "${registry}teastore-kieker-rabbitmq${tag}"
+  docker push "${registry}teastore-base${tag}"
+  docker push "${registry}teastore-registry${tag}"
+  docker push "${registry}teastore-persistence${tag}"
+  docker push "${registry}teastore-image${tag}"
+  docker push "${registry}teastore-webui${tag}"
+  docker push "${registry}teastore-auth${tag}"
+  docker push "${registry}teastore-recommender${tag}"
 fi
